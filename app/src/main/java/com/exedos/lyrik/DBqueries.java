fc410ffb3540a3cd7f0724e033002370a1f4700b
Exedos <sayhello@exedos.co>
2021-09-17 13:16:30 -0700

    version v1

---
+++ app/src/main/java/com/exedos/lyrik/DBqueries.java
@@ -0,0 +1,15 @@
+package com.exedos.lyrik;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DBqueries {
+
+
+
+    public static FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+    public static String email,fullname,profile;
+
+
+
+}
---
+++ app/src/main/java/com/exedos/lyrik/HomeListModel.java
@@ -0,0 +1,22 @@
+package com.exedos.lyrik;
+
+public class HomeListModel {
+    private String songTitle;
+
+    public HomeListModel(String songTitle) {
+
+        this.songTitle = songTitle;
+    }
+
+
+
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public void setSongTitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+
+
+}
